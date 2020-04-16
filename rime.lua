fce19028163ac2695d59b771fada306fc003c984
KyleBing <kylebing@163.com>
2020-04-16 20:02:06 +0800

    update

--- rime.lua
+++ rime.lua
@@ -1,13 +1,14 @@
 function date_translator(input, seg)
    if (input == "date") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "年月日"))
       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "年-月-日"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "年月日"))
    end
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), "时分"))
       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时分秒"))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), "时分"))
+	  yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), "长时间"))
    end
 end
 
